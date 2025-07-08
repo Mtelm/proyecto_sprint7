3f53bde244dc7969c87dd7da83ce2bc19b456018
Mtelm <tellez.manuel94@gmail.com>
2025-07-08 11:04:09 -0500

    fixed

--- app.py
+++ app.py
@@ -12,14 +12,14 @@ st.header('Proyecto Sprint 7')
  #boton para generar histograma
 hist_button=st.checkbox('Generar Histograma')
 Scatter_button=st.checkbox('Generar Gráfico de Dispersión')
-""" 
-if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    #crea un histograma
-    fig=px.histogram(car_data, x='odometer')
-    #mostrar un grafico interactivo con plotly
-    st.plotly_chart(fig, use_container_width=True)
-     """
+
+#if hist_button:
+#    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+#    #crea un histograma
+#    fig=px.histogram(car_data, x='odometer')
+#    #mostrar un grafico interactivo con plotly
+#    st.plotly_chart(fig, use_container_width=True)
+
 # metodo opcional
 if hist_button:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
